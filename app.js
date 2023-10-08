4daa45e5e7fbad03e3f35964e61adbeff1b02db3
Miguel Lopez <miguellopez32@gmail.com>
2023-10-07 22:50:38 -0300

    Agrego primeras imagenes de productos

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3000; // Puedes cambiar el puerto si lo deseas
+const port = 3000;
+
+// Require el archivo de productos desde la carpeta "server"
+const productos = require('./server/productos');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { productos });
 });
 
 app.listen(port, () => {
   console.log(`Servidor en ejecución en http://localhost:${port}`);
 });
+
+
---
+++ server/productos.js
@@ -0,0 +1,16 @@
+const productos = [
+    { imagen: '/images/producto1.jpeg' },
+    { imagen: '/images/producto2.jpeg' },
+    { imagen: '/images/producto3.jpeg' },
+    { imagen: '/images/producto4.jpeg' },
+    { imagen: '/images/producto5.jpeg' },
+    { imagen: '/images/producto6.jpeg' },
+    { imagen: '/images/producto7.jpeg' },
+    { imagen: '/images/producto8.jpeg' },
+    { imagen: '/images/producto9.jpeg' },
+    { imagen: '/images/producto0.jpeg' },
+    // Agrega más objetos con las rutas de las imágenes
+  ];
+  module.exports = productos;
+  
+  
