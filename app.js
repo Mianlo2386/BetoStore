17df8a7f1a3cc2728594ec8d5ae4b21cbc92a671
Miguel Lopez <miguellopez32@gmail.com>
2023-10-11 23:18:55 -0300

    Se agregan productos destacados

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Require el archivo de productos desde la carpeta "server"
+// Require los archivos de productos y productosDestacados desde la carpeta "server"
 const productos = require('./server/productos');
+const productosDestacados = require('./server/productosDestacados');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { productos });
+  res.render('index', { productos, productosDestacados }); // Pasa ambas variables a la vista
 });
 
 app.listen(port, () => {
   console.log(`Servidor en ejecución en http://localhost:${port}`);
 });
 
-app.get('/productos', (req, res) => {
-  res.render('productos', { productos });
-});
+// Resto de tus rutas...
+
+
+
 
 app.get('/buscar', (req, res) => {
   const query = req.query.q;
@@ -30,6 +32,8 @@ app.get('/buscar', (req, res) => {
   res.render('resultados', { resultados });
 });
 
-
+app.get('/contacto', (req, res) => {
+  res.render('contacto');
+});
 
 
---
+++ server/productosDestacados.js
@@ -0,0 +1,22 @@
+
+const productosDestacados = [
+    {
+        id: 2,
+        imagen: '/images/producto2.jpeg',
+        descripcion: 'Descripción del producto 2: Aquí va la descripción del producto.',
+        descuento: '10% OFF',
+     },
+     { 
+        id: 3,
+        imagen: '/images/producto3.jpeg',
+        descripcion: 'Descripción del producto 3: Aquí va la descripción del producto.',
+        descuento: '10% OFF',
+     },
+     {
+        id: 4,
+        imagen: '/images/producto4.jpeg',
+        descripcion: 'Descripción del producto 4: Aquí va la descripción del producto.',
+        descuento: '10% OFF',
+     },
+];
+module.exports = productosDestacados;
